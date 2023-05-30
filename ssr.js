5f5d696599d42cdf1b9e7ae2837e1caee6551c92
Lennart Jansen <lennart.official.jansen@gmail.com>
2023-05-30 20:26:48 +0200

    prompt package installed

--- ssr.js
+++ ssr.js
@@ -1,10 +1,15 @@
+'use strict';
 // pool of choices for the game
 const pool = ['Scissor', 'Rock', 'Paper'];
 
 // freeze makes the pool uneditable
 Object.freeze(pool);
-
 console.log(pool);
+
+//prompt-sync npm package stuff
+const prompt = require('prompt');
+prompt.start();
+// const prompt = promptSync();
 
 //Formula:  Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -13,6 +18,7 @@ function compare(a, b) {
   let res;
   if (a === b) {
     res = 0;
+    return res;
   }
   switch (a) {
     case 'Scissor':
@@ -37,5 +43,31 @@ function compare(a, b) {
         return res;
       }
       break;
+    case 'Paper':
+      if (b === 'Rock') {
+        res = -1;
+        return res;
+      }
+      if (b === 'Scissor') {
+        res = 1;
+        return res;
+      }
+      break;
   }
 }
+
+//------------------------------------------------------------------------------------
+// first testing
+// console.log(compare('Scissor', 'Scissor')); // 0
+// console.log(compare('Scissor', 'Rock')); // 1
+// console.log(compare('Scissor', 'Paper')); // -1
+
+// console.log(compare('Rock', 'Rock')); // 0
+// console.log(compare('Rock', 'Scissor')); // -1
+// console.log(compare('Rock', 'Paper')); // 1
+
+// console.log(compare('Paper', 'Paper')); // 0
+// console.log(compare('Paper', 'Rock')); // -1
+// console.log(compare('Paper', 'Scissor')); // 1
+//------------------------------------------------------------------------------------
+prompt.get('choice');
